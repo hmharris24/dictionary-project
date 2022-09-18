30e4af0883b5e337e9044ec93ef8848cb256a3ed
hmharris24 <102563834+hmharris24@users.noreply.github.com>
2022-09-17 22:34:25 -0500

    Added logo and updated footer

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import logo from "./logo.png";
 import "./App.css";
 import Dictionary from "./Dictionary";
 
@@ -5,12 +6,25 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">Dictionary</header>
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
         <main>
           <Dictionary defaultKeyword="sunset" />
         </main>
         <footer className="App-footer">
-          <small>Coded by Holly Meyers</small>
+          <small>
+            This website was coded by{" "}
+            <a href="https://www.linkedin.com/in/holly-meyers/">Holly Meyers</a>{" "}
+            and is{" "}
+            <a href="https://github.com/hmharris24/dictionary-project">
+              open-sourced on GitHub
+            </a>{" "}
+            and{" "}
+            <a href="https://subtle-mermaid-e919ba.netlify.app/">
+              hosted on Netlify.
+            </a>
+          </small>
         </footer>
       </div>
     </div>
